36269a6fca952f3326ce6e9ef8f09fdf089b0d49
Guillaume Isabelle <jgi@jgwill.com>
2019-04-11 11:31:25 -0400

    upgraded test

--- test/index.js
+++ test/index.js
@@ -2,6 +2,20 @@ import test from 'tape';
 import nsset from '../src';
 
 test('nsset', (t) => {
-  t.plan(1);
-  t.equal(true, nsset(), 'return true');
+   t.plan(1);
+   
+   
+   var txt = "hello world";
+   var target = "HOME.FOOTER.MYNOTES";
+   
+   var o = new Object();
+   
+   o.whatever = "something";
+   
+   var r = 
+   nsset(target,o,txt);
+   
+   
+   t.equal(txt, o.HOME.FOOTER.MYNOTES,txt);
+
 });
